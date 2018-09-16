6d7e3494bed869cd260722c13e830f60f5b6c167
David Edward Weirich <weirich.david@gmail.com>
2018-09-16 15:06:10 -0400

    Uses float linked list for positions

--- objloader.c
+++ objloader.c
@@ -2,25 +2,27 @@
 #include <stdlib.h>
 
 #include "matrix.h"
+#include "mesh.h"
 
-typedef struct Vector3fLL {
-    float x, y, z;
-    struct Vector3fLL *next;
-} Vector3fLL;
 
-typedef struct IndexLL {
+typedef struct FloatLL {
+    float x;
+    struct FloatLL *next;
+} FloatLL;
+
+typedef struct IntLL {
     int i;
-    struct IndexLL *next;
-} IndexLL;
+    struct IntLL *next;
+} IntLL;
 
 int main() {
-    Vector3fLL *vert_head = calloc(1, sizeof(Vector3fLL));
-    Vector3fLL *vert_tail = vert_head;
+    FloatLL *pos_head = calloc(1, sizeof(FloatLL));
+    FloatLL *pos_tail = pos_head;
 
-    IndexLL *ind_head = calloc(1, sizeof(IndexLL));
-    IndexLL *ind_tail = ind_head;
+    IntLL *ind_head = calloc(1, sizeof(FloatLL));
+    IntLL *ind_tail = ind_head;
 
-    int num_verts = 0;
+    int num_poss = 0;
     int num_inds = 0;
 
 
@@ -35,62 +37,38 @@ int main() {
     // Read obj file data into linked lists
     while(fgets(buffer, sizeof(buffer), fp)) {
         if (buffer[0] == 'v') {
-            float a, b, c;
-            sscanf(buffer, "v %f %f %f", &a, &b, &c);
+            float x[3];
+            sscanf(buffer, "v %f %f %f", x, x+1, x+2);
 
-            num_verts++;
-            
-            vert_tail->x = a;
-            vert_tail->y = b;
-            vert_tail->z = c;
-            vert_tail->next = calloc(1, sizeof(Vector3fLL));
-
-            vert_tail = vert_tail->next;
+            for(int i = 0; i < 3; i++) {
+                pos_tail->x = x[i];
+                pos_tail->next = calloc(1, sizeof(FloatLL));
+                pos_tail = pos_tail->next;
+            }
+            num_poss += 3;
         }
+
         if (buffer[0] == 'f') {
-            int a, b, c;            
+            int a[3];
+            sscanf(buffer, "f %d %d %d", a, a+1, a+2);
 
-            sscanf(buffer, "f %d %d %d", &a, &b, &c);
-
+            for(int i = 0; i < 3; i++) {
+                ind_tail->i = a[i];
+                ind_tail->next = calloc(1, sizeof(IntLL));
+                ind_tail = ind_tail->next;
+            }
             num_inds += 3;
-
-            ind_tail->i = a;
-            ind_tail->next = calloc(1, sizeof(IndexLL));
-            ind_tail = ind_tail->next;
-
-            ind_tail->i = b;
-            ind_tail->next = calloc(1, sizeof(IndexLL));
-            ind_tail = ind_tail->next;
-
-            ind_tail->i = c;
-            ind_tail->next = calloc(1, sizeof(IndexLL));
-            ind_tail = ind_tail->next;
         }
     }
 
     fclose(fp);
 
     printf("-----------\n");
-    printf("Verts: %d    Indices: %d\n", num_verts, num_inds);
+    printf("READ:\n");
+    printf("Verts: %d    Indices: %d\n", num_poss, num_inds);
     printf("-----------\n");
 
-    Vector3fLL *current = vert_head;
-
-    while (current->next) {
-        printf("(%f, %f, %f)\n", current->x, current->y, current->z);
-        Vector3fLL *next = current->next;
-        free(current);
-        current = next;
-    }
-
-    IndexLL *ind_current = ind_head;
-
-    while(ind_current->next) {
-        printf("%d, ", ind_current->i);
-        IndexLL *next = ind_current->next;
-        free(ind_current);
-        ind_current = next;
-    }
+    // Convert linked lists to normal arrays
 
     return 0;
 }
